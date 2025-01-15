98aba4ba7ebd48b6f18e44a9e6b89406f0580ab0
Atharva Ajagekar <atharva7ajagekar@gmail.com>
2025-01-15 13:12:04 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ dotenv.config({
     path: './env'
 })
 
+import { app } from './app.js';
 import connectDB from './db/index.js';
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log("app is listening on:",process.env.PORT);
+    })
+})
+.catch((error)=>{
+    console.log("mongodb connection error: ",error);
+})
 
 
 
